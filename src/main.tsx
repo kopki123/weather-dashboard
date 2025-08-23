5b6c331f9ce2c1d13092cbd25c93d4afdc2d8bf4
kopki <kopki684@gmail.com>
2025-08-23 22:40:27 +0800

    feat: 更新 GA 追蹤碼

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './i18n/i18n.ts';
 import './index.css';
 import App from './App.tsx';
 import { WeatherProvider } from './contexts/WeatherContext.tsx';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
